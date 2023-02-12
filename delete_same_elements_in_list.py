ed00154a4583ec69fb2203cc687b875182bfe109
hjl <111429201+ifhjl@users.noreply.github.com>
2023-02-12 11:01:58 +0800

    删除文件中某列重复的元素且将处理后的数据导出至原excel文件

---
+++ delete_same_elements_in_list.py
@@ -0,0 +1,7 @@
+import pandas as pd
+#name->list's name  path->excel_file's path
+def delete_same(name,path):
+  data = pd.read_excel(path)
+  data = data.drop_duplicates(subset=[name],keep="first")
+  data.to_excel(path)
+  
