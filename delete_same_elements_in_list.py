5b2a543a0cdd425502c8ad089b7b7b881ea900d0
hjl <111429201+ifhjl@users.noreply.github.com>
2023-02-12 15:51:18 +0800

    Update delete_same_elements_in_list.py

--- delete_same_elements_in_list.py
+++ delete_same_elements_in_list.py
@@ -3,5 +3,5 @@ import pandas as pd
 def delete_same(name,path):
   data = pd.read_excel(path)
   data = data.drop_duplicates(subset=[name],keep="first")
-  data.to_excel(path)
+  data.to_excel(path,index=False)
   
