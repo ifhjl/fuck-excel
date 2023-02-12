720617ad315251677b8466eaa19a2bd5cec8e3cc
hjl <111429201+ifhjl@users.noreply.github.com>
2023-02-12 10:54:32 +0800

    删除文件中某列的空格，以防排序异常
    两个形参分别为列的名字与文件的路径

---
+++ delete_blank_things.py
@@ -0,0 +1,21 @@
+import pandas as pd    
+import re
+ 
+def clear(name,path):
+    data=pd.read_excel(path)
+    sg.Print(path)
+    count=0
+    boo=1
+    for i in data[name]:
+        length=len(i)
+        if(i[length-1]==" "):
+            data[name][count]=i[:length-1]
+            sg.Print("名字后有空格的为：{}".format(i))
+            boo=0
+        count=count+1
+    data.to_excel(path)
+    if(boo==1):
+        print("数据正常，无需修改")
+    else:
+        print("已完成，修改后的文件为{}".format(path))
+
