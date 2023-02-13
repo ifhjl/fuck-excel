60539a1cdc15fd634307d78a7362d55c3902b4b8
hjl <111429201+ifhjl@users.noreply.github.com>
2023-02-13 10:41:38 +0800

    不改变顺序的前提下修改排序

--- specialize.py
+++ specialize.py
@@ -1,12 +1,10 @@
-#本程序对特征表进行排序，并从0到1进行排序，如需增加或修改功能请自行更改
+#本程序对特征表进行排序，并从0开始进行排序，如需增加或修改功能请自行更改
 import pandas as pd
 
 
 def specialize(name,path):
     data = pd.read_excel(path)
-    print("改列的元素按排序的结果为:{}，将按照由0开始的排序对其进行特征化。".format(sorted(data[name].unique())))
-    data[name] = sorted(data[name])
-    for i in range(len(data[name].unique())):
-        data[name] = data[name].replace({data[name].unique()[i]:i})
+    #此方法可使目标文件的顺序不变且按照升序进行排序
+    data[name] = data[name].replace([i for i in data.sort_values(name)[name].unique()],[j for j in range(len(data[name].unique()))])
     data.to_excel(path,index=False)
     print("完成")
